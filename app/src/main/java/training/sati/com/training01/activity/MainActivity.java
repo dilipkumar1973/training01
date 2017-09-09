a0ca1c7b5b17e5be1aa9699af14b7264c1e59262
Dilip <dilipkumar_1973@yahoo.com>
2017-09-09 17:11:30 +0530

    modified MainActivity

--- app/src/main/java/training/sati/com/training01/activity/MainActivity.java
+++ app/src/main/java/training/sati/com/training01/activity/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.context = getBaseContext();
         setContentView(R.layout.layout_1);
-
+        System.out.println("Just a msg");
     }
 
     public void onClick(View view){
